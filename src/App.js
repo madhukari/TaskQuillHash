b0e6b82a0c88d47d11146509ec8af8297f7d8012
madhukiranka <62378655+madhukiranka@users.noreply.github.com>
2020-07-02 19:00:24 +0530

    implemented line chart with static data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Contacts from './SampleApi/Contacts';
+import Dropdown from './DropDowns/Dropdown';
+import LineChart from './Charts/LineChart';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    class App extends Component {
 
-export default App;
+      state = {
+        contacts: []
+      }
+
+      componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then((data) => {
+          this.setState({ contacts: data })
+        })
+        .catch(console.log)
+      }
+      render () {
+        return (
+          // JSX to render goes here...
+          <div>
+            {/* <Dropdown /> */}
+            {/* <Contacts contacts={this.state.contacts} />  */}
+            <LineChart/>
+            
+          </div>
+        );
+      }      
+    }
+
+    export default App;
